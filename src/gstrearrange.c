b1aca87103f15f3dcc5a9d605245ab1d39dd3f9d
Manuel Reithuber <git@manuel.reithuber.net>
2012-03-07 20:21:09 +0100

    implemented the basic functionality of gst-rearrange

---
+++ src/gstrearrange.c
@@ -0,0 +1,361 @@
+/*
+ * GStreamer
+ * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
+ * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
+ * Copyright (C) 2012 Manuel Reithuber <gstreamer@manuel.reithuber.net>>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:element-rearrange
+ *
+ * Rearrange spreads two input channels to two channels of a 5.1 or 7.1 signal.
+ * It therefore allows you to send more than one stereo signals to multichannel
+ * sound cards.
+ *
+ * Keep in mind that some audio backends do up-/downmixing if the signal's
+ * channel count doesn't match the channel count of the audio device
+ *
+ * <refsect2>
+ * <title>Example launch line</title>
+ * |[
+ * gst-launch -v -m fakesrc ! rearrange ! fakesink silent=TRUE
+ * ]|
+ * </refsect2>
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+
+#include "gstrearrange.h"
+
+GST_DEBUG_CATEGORY_STATIC (gst_re_arrange_debug);
+#define GST_CAT_DEFAULT gst_re_arrange_debug
+
+/* Filter signals and args */
+enum
+{
+  /* FILL ME */
+  LAST_SIGNAL
+};
+
+enum
+{
+  PROP_0,
+  PROP_SILENT
+};
+
+#if 0
+#define SRCCAPS \
+  "audio/x-raw-int, " \
+	"width = (int) 16, " \
+	"depth = (int) 16, " \
+	"endianness = (int) BYTE_ORDER, " \
+	"channels = (int) { 8 }, " \
+	"rate = (int) [ 8000, 96000 ]"
+
+
+#define SINKCAPS \
+  "audio/x-raw-int, " \
+	"width = (int) 16, " \
+	"depth = (int) 16, " \
+	"endianness = (int) BYTE_ORDER, " \
+	"channels = (int) { 2 }, " \
+	"rate = (int) [ 8000, 96000 ]"
+#else
+#define SRCCAPS \
+  "audio/x-raw-int, " \
+	"width = (int) 16, " \
+	"depth = (int) 16, " \
+	"endianness = (int) BYTE_ORDER, " \
+	"channels = (int) { 8 }, " \
+	"rate = (int) 48000"
+
+#define SINKCAPS \
+  "audio/x-raw-int, " \
+	"width = (int) 16, " \
+	"depth = (int) 16, " \
+	"endianness = (int) BYTE_ORDER, " \
+	"channels = (int) { 2 }, " \
+	"rate = (int) 48000"
+
+#endif
+
+/* the capabilities of the inputs and outputs.
+ *
+ * describe the real formats here.
+ */
+static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE ("sink",
+	GST_PAD_SINK,
+	GST_PAD_ALWAYS,
+	GST_STATIC_CAPS(SINKCAPS)
+	);
+
+static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src",
+	GST_PAD_SRC,
+	GST_PAD_ALWAYS,
+	GST_STATIC_CAPS(SRCCAPS)
+	);
+
+GST_BOILERPLATE (GstReArrange, gst_re_arrange, GstElement,
+	GST_TYPE_ELEMENT);
+
+static void gst_re_arrange_set_property (GObject * object, guint prop_id,
+	const GValue * value, GParamSpec * pspec);
+static void gst_re_arrange_get_property (GObject * object, guint prop_id,
+	GValue * value, GParamSpec * pspec);
+
+static gboolean gst_re_arrange_set_caps (GstPad * pad, GstCaps * caps);
+static GstFlowReturn gst_re_arrange_chain (GstPad * pad, GstBuffer * buf);
+
+/* GObject vmethod implementations */
+
+static void
+gst_re_arrange_base_init (gpointer gclass)
+{
+  GstElementClass *element_class = GST_ELEMENT_CLASS (gclass);
+
+  gst_element_class_set_details_simple(element_class,
+	"ReArrange",
+	"Re-arrange audio channels",
+	"Move the two channels of an incoming stereo signal to two of the other channels (e.g. rear, side or center/lfe)",
+	"Manuel Reithuber <gstreamer@manuel.reithuber.net>");
+
+  gst_element_class_add_pad_template (element_class,
+	  gst_static_pad_template_get (&src_factory));
+  gst_element_class_add_pad_template (element_class,
+	  gst_static_pad_template_get (&sink_factory));
+}
+
+/* initialize the rearrange's class */
+static void
+gst_re_arrange_class_init (GstReArrangeClass * klass)
+{
+  GObjectClass *gobject_class;
+  GstElementClass *gstelement_class;
+
+  gobject_class = (GObjectClass *) klass;
+  gstelement_class = (GstElementClass *) klass;
+
+  gobject_class->set_property = gst_re_arrange_set_property;
+  gobject_class->get_property = gst_re_arrange_get_property;
+
+  g_object_class_install_property (gobject_class, PROP_SILENT,
+	  g_param_spec_boolean ("silent", "Silent", "Produce verbose output ?",
+		  FALSE, G_PARAM_READWRITE));
+}
+
+/* initialize the new element
+ * instantiate pads and add them to element
+ * set pad calback functions
+ * initialize instance structure
+ */
+static void
+gst_re_arrange_init (GstReArrange * filter,
+	GstReArrangeClass * gclass)
+{
+  filter->sinkpad = gst_pad_new_from_static_template (&sink_factory, "sink");
+  gst_pad_set_setcaps_function (filter->sinkpad,
+								GST_DEBUG_FUNCPTR(gst_re_arrange_set_caps));
+  gst_pad_set_getcaps_function (filter->sinkpad,
+								GST_DEBUG_FUNCPTR(gst_pad_proxy_getcaps));
+  gst_pad_set_chain_function (filter->sinkpad,
+							  GST_DEBUG_FUNCPTR(gst_re_arrange_chain));
+
+  filter->srcpad = gst_pad_new_from_static_template (&src_factory, "src");
+  gst_pad_set_getcaps_function (filter->srcpad,
+								GST_DEBUG_FUNCPTR(gst_pad_proxy_getcaps));
+
+  gst_element_add_pad (GST_ELEMENT (filter), filter->sinkpad);
+  gst_element_add_pad (GST_ELEMENT (filter), filter->srcpad);
+  filter->silent = FALSE;
+}
+
+static void
+gst_re_arrange_set_property (GObject * object, guint prop_id,
+	const GValue * value, GParamSpec * pspec)
+{
+  GstReArrange *filter = GST_REARRANGE (object);
+
+  switch (prop_id) {
+	case PROP_SILENT:
+	  filter->silent = g_value_get_boolean (value);
+	  break;
+	default:
+	  G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+	  break;
+  }
+}
+
+static void
+gst_re_arrange_get_property (GObject * object, guint prop_id,
+	GValue * value, GParamSpec * pspec)
+{
+  GstReArrange *filter = GST_REARRANGE (object);
+
+  switch (prop_id) {
+	case PROP_SILENT:
+	  g_value_set_boolean (value, filter->silent);
+	  break;
+	default:
+	  G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+	  break;
+  }
+}
+
+/* GstElement vmethod implementations */
+
+/* this function handles the link with other elements */
+static gboolean
+gst_re_arrange_set_caps (GstPad * pad, GstCaps * caps)
+{
+  GstReArrange *filter;
+  GstPad *otherpad;
+
+  filter = GST_REARRANGE (gst_pad_get_parent (pad));
+  otherpad = (pad == filter->srcpad) ? filter->sinkpad : filter->srcpad;
+  gst_object_unref (filter);
+
+  return gst_pad_set_caps (otherpad, caps);
+}
+
+
+/*
+  POS.:
+	0 front
+	1 rear
+	2 center/lfe
+	3 side
+*/
+/* chain function
+ * this function does the actual processing
+ */
+static GstFlowReturn
+gst_re_arrange_chain (GstPad * pad, GstBuffer * buf)
+{
+	int tgtChannels = 8, width = 2, pos = 3;
+	GstReArrange *filter = GST_REARRANGE (GST_OBJECT_PARENT (pad));
+
+	GstBuffer *tgtBuf = gst_buffer_new_and_alloc(buf->size*(tgtChannels/2));
+
+#if 0
+	GstCaps *tgtCaps = gst_buffer_get_caps(buf);
+#else
+	GstCaps *tgtCaps = gst_caps_from_string(
+		"audio/x-raw-int,"
+			"endianness=(int)1234,"
+			"signed=(boolean)true,"
+			"width=(int)16,"
+			"depth=(int)16,"
+			"rate=(int)44100,"
+			"channels=(int)8");
+#endif
+
+	gst_buffer_set_caps(tgtBuf, tgtCaps);
+	//gst_buffer_set_data(tgtBuf, buf->data, buf->size*2);
+
+	gint8 *pSrc = buf->data, *pTgt = tgtBuf->data;
+
+	int curByte = 0, fromByte = 2*pos*width;
+	int toByte = fromByte + (width*2); // two channels
+	while (pTgt < tgtBuf->data + tgtBuf->size) {
+		//g_print("curByte: %i ", curByte);
+		//gst_caps_get_structure()
+		if (curByte >= fromByte && curByte < toByte) {
+			*pTgt++ = *pSrc++;
+			//g_print("ch\n");
+		}
+		else {
+			*pTgt++ = 0;
+			//g_print("silence\n");
+		}
+		curByte = (curByte+1) % (tgtChannels*width);
+	}
+
+	//gst_buffer_set_caps(tgtBuf->caps, gst_caps_from_string(SRCCAPS));
+	return gst_pad_push(filter->srcpad, tgtBuf);
+}
+
+
+/* entry point to initialize the plug-in
+ * initialize the plug-in itself
+ * register the element factories and other features
+ */
+static gboolean
+rearrange_init (GstPlugin * rearrange)
+{
+  /* debug category for fltering log messages
+   *
+   * exchange the string 'Template rearrange' with your description
+   */
+  GST_DEBUG_CATEGORY_INIT (gst_re_arrange_debug, "rearrange",
+	  0, "Template rearrange");
+
+  return gst_element_register (rearrange, "rearrange", GST_RANK_NONE,
+	  GST_TYPE_REARRANGE);
+}
+
+/* PACKAGE: this is usually set by autotools depending on some _INIT macro
+ * in configure.ac and then written into and defined in config.h, but we can
+ * just set it ourselves here in case someone doesn't use autotools to
+ * compile this code. GST_PLUGIN_DEFINE needs PACKAGE to be defined.
+ */
+#ifndef PACKAGE
+#define PACKAGE "myfirstrearrange"
+#endif
+
+/* gstreamer looks for this structure to register rearranges
+ *
+ * exchange the string 'Template rearrange' with your rearrange description
+ */
+GST_PLUGIN_DEFINE (
+	GST_VERSION_MAJOR,
+	GST_VERSION_MINOR,
+	"rearrange",
+	"Template rearrange",
+	rearrange_init,
+	VERSION,
+	"LGPL",
+	"GStreamer",
+	"http://gstreamer.net/"
+)
